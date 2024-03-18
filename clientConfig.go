b5c8d5050772578bb33994b3e8437535dc922dcf
krshna-dtdl <129184791+krshna-dtdl@users.noreply.github.com>
2024-03-18 18:04:55 +0530

    Add TLS | Talaria - Petasos support

--- clientConfig.go
+++ clientConfig.go
@@ -1,14 +1,19 @@
 package kratos
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"errors"
+	"fmt"
+	"github.com/xmidt-org/wrp-go/v3"
+	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/xmidt-org/sallust"
-	"github.com/xmidt-org/wrp-go/v3"
 	"go.uber.org/zap"
 )
 
@@ -40,6 +45,9 @@ type ClientConfig struct {
 	HandlePingMiss       HandlePingMiss
 	ClientLogger         *zap.Logger
 	PingConfig           PingConfig
+	UseSSL               bool
+	CertificatesPath     string
+	PetasosEnabled       bool
 }
 
 // QueueConfig is used to configure all the queues used to make kratos asynchronous.
@@ -66,7 +74,7 @@ func NewClient(config ClientConfig) (Client, error) {
 		manufacturer: config.Manufacturer,
 	}
 
-	newConnection, connectionURL, err := createConnection(inHeader, config.DestinationURL)
+	newConnection, connectionURL, err := createConnection(inHeader, config)
 
 	if err != nil {
 		return nil, err
@@ -81,7 +89,7 @@ func NewClient(config ClientConfig) (Client, error) {
 
 	// at this point we know that the URL connection is legitimate, so we can do some string manipulation
 	// with the knowledge that `:` will be found in the string twice
-	connectionURL = connectionURL[len("ws://"):strings.LastIndex(connectionURL, ":")]
+	connectionURL = strings.TrimPrefix(connectionURL[len("ws://"):], ":")
 
 	var logger *zap.Logger
 	if config.ClientLogger != nil {
@@ -134,11 +142,27 @@ func NewClient(config ClientConfig) (Client, error) {
 }
 
 // private func used to generate the client that we're looking to produce
-func createConnection(headerInfo *clientHeader, httpURL string) (connection *websocket.Conn, wsURL string, err error) {
+func createConnection(headerInfo *clientHeader, config ClientConfig) (connection *websocket.Conn, wsURL string, err error) {
 	_, err = wrp.ParseDeviceID(headerInfo.deviceName)
 
 	if err != nil {
 		return nil, "", err
+	}
+	var talariaInstance = ""
+	tlsConfig := GetTLSConfig(strings.Split(config.DeviceName, ":")[1], config.CertificatesPath, config.UseSSL)
+	if config.PetasosEnabled {
+		talariaInstance, err = getTalariaInstance(config, tlsConfig)
+		if err != nil {
+			fmt.Println("Error while fetching Talaria for: ", config.DeviceName, err)
+			return nil, "", err
+		}
+	} else {
+		talariaInstance = config.DestinationURL
+	}
+
+	dialer := &websocket.Dialer{}
+	if config.UseSSL {
+		dialer.TLSClientConfig = tlsConfig
 	}
 
 	// make a header and put some data in that (including MAC address)
@@ -150,16 +174,17 @@ func createConnection(headerInfo *clientHeader, httpURL string) (connection *web
 	headers.Add("X-Webpa-Manufacturer", headerInfo.manufacturer)
 
 	// make sure destUrl's protocol is websocket (ws)
-	wsURL = strings.Replace(httpURL, "http", "ws", 1)
+	wsURL = strings.Replace(talariaInstance, "http", "ws", 1)
 
 	// creates a new client connection given the URL string
-	connection, resp, err := websocket.DefaultDialer.Dial(wsURL, headers)
+	connection, resp, err := dialer.Dial(wsURL, headers)
 
 	for err == websocket.ErrBadHandshake && resp != nil && resp.StatusCode == http.StatusTemporaryRedirect {
+		fmt.Println(err)
 		// Get url to which we are redirected and reconfigure it
 		wsURL = strings.Replace(resp.Header.Get("Location"), "http", "ws", 1)
 
-		connection, resp, err = websocket.DefaultDialer.Dial(wsURL, headers)
+		connection, resp, err = dialer.Dial(wsURL, headers)
 	}
 
 	if err != nil {
@@ -170,4 +195,92 @@ func createConnection(headerInfo *clientHeader, httpURL string) (connection *web
 	}
 
 	return connection, wsURL, nil
+}
+
+func getTalariaInstance(config ClientConfig, tlsConfig *tls.Config) (string, error) {
+
+	// Create HTTP client with custom transport
+
+	tr := &http.Transport{
+		TLSClientConfig: func() *tls.Config {
+			if config.UseSSL {
+				return tlsConfig
+			} else {
+				return nil
+			}
+		}(),
+		DisableCompression: true,
+		DisableKeepAlives:  true,
+	}
+	client := &http.Client{Transport: tr, CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}}
+
+	// Create HTTP request with headers
+	req, err := http.NewRequest("GET", config.DestinationURL, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("X-Webpa-Device-Name", config.DeviceName)
+
+	// Send HTTP request and get response
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusTemporaryRedirect {
+		return "", errors.New("petasos didn't respond")
+	}
+
+	if resp.Header.Get("Location") != "" {
+		return resp.Header.Get("Location"), nil
+	}
+
+	// Read response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	pattern := `((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s!()\[\]{};:\'".,<>?«»“”‘’]))`
+
+	re := regexp.MustCompile(pattern)
+	match := re.FindString(string(body))
+	return match, nil
+}
+
+func GetTLSConfig(macAddress string, certificatesPath string, useSSL bool) *tls.Config {
+	if !useSSL {
+		return nil
+	}
+	certFile := fmt.Sprintf("%s/%s-client.crt", certificatesPath, macAddress)
+	keyFile := fmt.Sprintf("%s/%s-key.pem", certificatesPath, macAddress)
+	caFile := fmt.Sprintf("%s/ca.crt", certificatesPath)
+
+	// Try reading the certificate files with the prefix of the provided macaddress
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		// If that fails, try reading the certificate files without the prefix
+		certFile = fmt.Sprintf("%s/client.crt", certificatesPath)
+		keyFile = fmt.Sprintf("%s/key.pem", certificatesPath)
+		cert, err = tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+	}
+
+	caCert, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		return nil
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	return &tls.Config{
+		RootCAs:      caCertPool,
+		Certificates: []tls.Certificate{cert},
+	}
+
 }
